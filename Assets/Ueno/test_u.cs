4684a43fc342600bb893ffdc036dca1a49da0ce6
unknown <mikanrin913@yahoo.co.jp>
2016-10-10 04:39:12 +0900

    fail add

---
+++ Assets/Ueno/test_u.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_u : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(0, 1, 0);
+    }
+}

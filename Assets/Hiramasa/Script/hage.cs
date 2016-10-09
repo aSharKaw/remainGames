b8642adad882516669144b6a2e9d4e0146c08d8a
pedius1175 <hiramasa1175@gmail.com>
2016-10-10 02:54:05 +0900

    私は禿げです。

---
+++ Assets/Hiramasa/Script/hage.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class hage : MonoBehaviour
+{
+
+	void Start ()
+  {
+
+	}
+	
+	void Update ()
+  {
+    UnityEngine.Debug.Log("私ははげです。");
+	}
+
+}
+

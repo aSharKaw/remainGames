62e4b3a2d924c605fc764dfcd3e89514499c054f
xSYUxx <syu_neko06@yahoo.co.jp>
2016-10-10 04:24:06 +0900

    Revert "Revert "追加""
    This reverts commit c785cddfac2030fd40051c735abbc38c22520550.

---
+++ Assets/Wada/Script/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        UnityEngine.Debug.Log("どーも！おはこんばんにちは　");
+	
+	}
+}

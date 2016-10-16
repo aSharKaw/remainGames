545514bb0aa3a5663588973a26b701fdc73b233a
xSYUxx <syu_neko06@yahoo.co.jp>
2016-10-17 03:12:06 +0900

    仮リザルトシーンの作成

---
+++ Assets/Arakawa/Script/resultManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resultManager : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 position = Vector2.zero;
+    [SerializeField]
+    private Vector2 size = Vector2.zero;
+
+    private string PlayerName = "hoge";
+
+    void OnGUI()
+    {
+        PlayerName = GUI.TextField(new Rect(position, size), PlayerName, 5);
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}

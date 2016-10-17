70135d47790d288b4b22fca9d1a3f782e6c06213
Uenonono <mikanrin913@yahoo.co.jp>
2016-10-17 23:46:14 +0900

    aomori

---
+++ Assets/Ueno/Script/DestryTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestryTime : MonoBehaviour
+{
+    [SerializeField]
+    private float time_D;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, time_D);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

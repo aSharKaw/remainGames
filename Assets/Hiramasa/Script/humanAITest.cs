7172d7694feb9f3379502d3af784509eae013817
pedius1175 <hiramasa1175@gmail.com>
2016-10-17 12:10:37 +0900

    add

---
+++ Assets/Hiramasa/Script/humanAITest.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class humanAITest : MonoBehaviour
+{
+
+  [SerializeField]
+  private Transform target;
+
+  private NavMeshAgent navMeshAgent;
+
+  void Start()
+  {
+    navMeshAgent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update()
+  {
+    navMeshAgent.SetDestination(target.position);
+  }
+
+}

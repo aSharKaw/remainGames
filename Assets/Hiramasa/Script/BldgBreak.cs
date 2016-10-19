8139074670f61d8564d964e15221db624aaaa92f
pedius1175 <hiramasa1175@gmail.com>
2016-10-20 01:18:11 +0900

    Bldgをspaceで破壊

---
+++ Assets/Hiramasa/Script/BldgBreak.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class BldgBreak : MonoBehaviour
+{
+
+  Rigidbody rb;
+
+  [SerializeField]
+  private bool breakBldgFlag;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void Update()
+  {
+
+    if (Input.GetKey(KeyCode.Space) && breakBldgFlag)
+    {
+      rb.isKinematic = false;
+    }
+
+  }
+
+}

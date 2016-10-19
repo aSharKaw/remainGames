c214d8e0778ac6fe2575c0291f033b4c40e1e2dd
pedius1175 <hiramasa1175@gmail.com>
2016-10-20 06:45:47 +0900

    6:45Push

---
+++ Assets/Hiramasa/Script/particleTest.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class particleTest : MonoBehaviour
+{
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+
+  void OnParticleCollision(GameObject obj)
+  {
+    Debug.Log("name " + obj.name);
+  }
+
+}

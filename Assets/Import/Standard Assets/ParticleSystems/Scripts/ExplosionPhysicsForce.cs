4a03f53fdc3e11454c852833daa257fd1ce9994d
pedius1175 <hiramasa1175@gmail.com>
2016-10-20 03:49:58 +0900

    ScpritAdd

--- Assets/Import/Standard Assets/ParticleSystems/Scripts/ExplosionPhysicsForce.cs
+++ Assets/Import/Standard Assets/ParticleSystems/Scripts/ExplosionPhysicsForce.cs
@@ -1,20 +1,16 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 1ece30c7551313b90e7cb2ebd19a9a123bd4be17
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
 
 namespace UnityStandardAssets.Effects
 {
   public class ExplosionPhysicsForce : MonoBehaviour
   {
-
     public float explosionForce = 4;
+
 
     private IEnumerator Start()
     {
@@ -27,7 +23,6 @@ namespace UnityStandardAssets.Effects
       float r = 10 * multiplier;
       var cols = Physics.OverlapSphere(transform.position, r);
       var rigidbodies = new List<Rigidbody>();
-
       foreach (var col in cols)
       {
         if (col.attachedRigidbody != null && !rigidbodies.Contains(col.attachedRigidbody))
@@ -35,45 +30,10 @@ namespace UnityStandardAssets.Effects
           rigidbodies.Add(col.attachedRigidbody);
         }
       }
-
       foreach (var rb in rigidbodies)
       {
         rb.AddExplosionForce(explosionForce * multiplier, transform.position, r, 1 * multiplier, ForceMode.Impulse);
       }
-
     }
   }
-=======
-namespace UnityStandardAssets.Effects
-{
-    public class ExplosionPhysicsForce : MonoBehaviour
-    {
-        public float explosionForce = 4;
-
-
-        private IEnumerator Start()
-        {
-            // wait one frame because some explosions instantiate debris which should then
-            // be pushed by physics force
-            yield return null;
-
-            float multiplier = GetComponent<ParticleSystemMultiplier>().multiplier;
-
-            float r = 10*multiplier;
-            var cols = Physics.OverlapSphere(transform.position, r);
-            var rigidbodies = new List<Rigidbody>();
-            foreach (var col in cols)
-            {
-                if (col.attachedRigidbody != null && !rigidbodies.Contains(col.attachedRigidbody))
-                {
-                    rigidbodies.Add(col.attachedRigidbody);
-                }
-            }
-            foreach (var rb in rigidbodies)
-            {
-                rb.AddExplosionForce(explosionForce*multiplier, transform.position, r, 1*multiplier, ForceMode.Impulse);
-            }
-        }
-    }
->>>>>>> 1ece30c7551313b90e7cb2ebd19a9a123bd4be17
 }

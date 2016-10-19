1ece30c7551313b90e7cb2ebd19a9a123bd4be17
hiramasa <hiramasa1175@gmail.com>
2016-10-20 03:19:08 +0900

    スタンダートアセット追加 (#27)
    * ファイル削除
    
    * ファイル削除
    
    * 新規作成
    
    * 一旦削除
    
    * また新規作成
    
    * Delete
    
    * Delete
    
    * Delete
    
    * test
    
    * Delete
    
    * Pushしてみる
    
    * pushTest
    
    * TarningPointAdd
    
    * Bldgをspaceで破壊
    
    * 一旦バックアップ
    
    * AssetsAdd

---
+++ Assets/Hiramasa/Script/BldgBreak.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class bldgBreak : MonoBehaviour
+{
+
+  Rigidbody rb;
+
+  [SerializeField]
+  private bool breakBldgFlag;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void Update()
+  {
+
+    if (Input.GetKey(KeyCode.Space) && breakBldgFlag)
+    {
+      rb.isKinematic = false;
+    }
+
+  }
+
+}

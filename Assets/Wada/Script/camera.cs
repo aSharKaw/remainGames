7eae9df1f403abb3fc4741804af55472fd36ca6a
PS14 <phantasystar2115@yahoo.co.jp>
2016-10-28 17:13:48 +0900

    test

---
+++ Assets/Wada/Script/camera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour
+{
+   
+    int x = 50;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+       if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 10, -10), 10.0f);
+            transform.Rotate(x, 0, 0);
+
+        }
+
+       if(Input.GetKeyDown((KeyCode.DownArrow)))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 3, -10), 10.0f);
+            transform.Rotate(-x, 0, 0);
+
+        }
+
+    }
+}

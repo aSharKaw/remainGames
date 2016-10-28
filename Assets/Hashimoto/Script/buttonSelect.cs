2f57d2d3ce7bc7e5d5665666e023ed9525fd59ac
pedius1175 <hiramasa1175@gmail.com>
2016-10-28 19:29:35 +0900

    ファイル名修正

---
+++ Assets/Hashimoto/Script/buttonSelect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class buttonSelect : MonoBehaviour
+{
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    GetComponent<Button>().Select();
+  }
+
+}

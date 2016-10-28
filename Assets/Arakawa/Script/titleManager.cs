16816e32590b9c732d2978d4147195b53ef8757d
xSYUxx <syu_neko06@yahoo.co.jp>
2016-10-28 15:47:09 +0900

    タイトルを仮タイトルから確定版に変更

--- Assets/Arakawa/Script/titleManager.cs
+++ Assets/Arakawa/Script/titleManager.cs
@@ -9,18 +9,49 @@ public class titleManager : MonoBehaviour {
 
     [SerializeField]
     private GameObject building;
+    [SerializeField]
+    private GameObject explosion;
 
     private Vector3 buildingPosition;
-
     private int count;
 
+    private int transitionTime;
+    private bool transition;
+
     //タップされればいいだけなので、細かい座標を取るのは割愛
+
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         count = 0;
         //building.transform.eulerAngles = new Vector3(90, 0, 0);
         buildingPosition = new Vector3(Random.Range(-15, 15), -3, Random.Range(-10, 10));
+
+        transition = false;
+        transitionTime = 30 * 60;
     }
+
+    void Transition(bool _transition)
+    {
+        int _count = 0;
+        _transition = true;
+        while(_transition)
+        {
+            //チュートリアル確認ウインドウ表示に差し替えること
+            Instantiate(explosion, new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)), Quaternion.Euler(Vector3.zero));
+
+            if (_count == transitionTime)
+            {
+                _transition = false;
+            }
+            else
+            { _count++; }
+            break;
+        }
+
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -29,15 +60,14 @@ public class titleManager : MonoBehaviour {
         {
             Instantiate(building, buildingPosition, building.transform.localRotation);
             buildingPosition = new Vector3(Random.Range(-15, 15), -3, Random.Range(-10, 10));
+
             count = 0;
         }
 
         if (Input.GetMouseButtonDown(0))
         {
+            Transition(transition);
             Debug.Log("Title Taped");
-            //チュートリアル確認ウインドウ表示に差し替えること
-
-            UnityEngine.SceneManagement.SceneManager.LoadScene("main");
         }
         else
         {

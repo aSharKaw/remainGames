17510f062c212efa00a250bcbfb7fb6b6dc8df24
xSYUxx <syu_neko06@yahoo.co.jp>
2016-11-05 02:18:30 +0900

    リザルトの変更。名前を保存しました

--- Assets/Arakawa/Script/resultManager.cs
+++ Assets/Arakawa/Script/resultManager.cs
@@ -1,26 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class resultManager : MonoBehaviour {
+public class resultManager : MonoBehaviour
+{
 
     [SerializeField]
     private Vector2 position = Vector2.zero;
     [SerializeField]
     private Vector2 size = Vector2.zero;
 
-    private string PlayerName = "hoge";
+    //テストテキスト
+    [SerializeField]
+    private UnityEngine.UI.Text nameText;
+
+    private string playerName = "hoge";
 
     void OnGUI()
     {
-        PlayerName = GUI.TextField(new Rect(position, size), PlayerName, 5);
+        playerName = GUI.TextField(new Rect(position, size), playerName, 5);
     }
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //入力テスト
+        //#if UNITY_EDITER
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            nameText.text = playerName;
+        }
+        //#endif
+    }
+
+    public void NameSave()
+    {
+        nameText.text = playerName;
+    }
 }
+
